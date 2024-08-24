d83cf8eeddb366c191c16d4ecf903d3fc9b49236
hamza <hamzasiddiq755@gmail.com>
2024-08-24 18:46:13 +0500

    start working on dashboard

---
+++ src/pages/ZMHDashboard/index.tsx
@@ -0,0 +1,156 @@
+import Lucide from "@/components/Base/Lucide";
+import { Menu } from "@/components/Base/Headless";
+import ReportBarChart6 from "@/components/ReportBarChart6";
+import ReportRadarChart from "@/components/ReportRadarChart";
+import { FormSelect } from "@/components/Base/Form";
+import Tippy from "@/components/Base/Tippy";
+import Button from "@/components/Base/Button";
+import Litepicker from "@/components/Base/Litepicker";
+import { useState } from "react";
+import clsx from "clsx";
+import _ from "lodash";
+
+function Main() {
+  return (
+    <div className="grid grid-cols-12 gap-y-10 gap-x-6">
+      <div className="col-span-12 xl:col-span-8">
+        <div>
+          <div className="flex flex-col md:h-10 gap-y-3 md:items-center md:flex-row">
+            <div className="text-base font-medium group-[.mode--light]:text-white">
+              General Report
+            </div>
+          </div>
+          <div className="p-5 mt-3.5 box ">
+            <div className="flex flex-col lg:items-center lg:flex-row gap-y-5">
+              <div className="flex flex-col sm:items-center sm:flex-row gap-x-3 gap-y-2">
+                Hellow Table
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="col-span-12 md:col-span-6 xl:col-span-4">
+        <div>
+          <div className="flex flex-col md:h-10 gap-y-3 md:items-center md:flex-row">
+            <div className="text-base font-medium xl:group-[.mode--light]:text-white">
+              Patient Overview
+            </div>
+          </div>
+          <div className="p-5 mt-3.5 box box--stacked">
+            <div className="flex flex-col gap-3 sm:items-center sm:flex-row">
+              <div>
+                <div className="text-xl font-medium">24,782</div>
+                <div className="mt-1 text-base text-slate-500">
+                  Total Patients
+                </div>
+              </div>
+              <div className="relative sm:ml-auto">
+                <Lucide
+                  icon="CalendarCheck2"
+                  className="absolute inset-y-0 left-0 z-10 w-4 h-4 my-auto ml-3 stroke-[1.3]"
+                />
+                <FormSelect className="sm:w-32 pl-9">
+                  <option value="daily">Daily</option>
+                  <option value="weekly">Weekly</option>
+                  <option value="monthly">Monthly</option>
+                  <option value="yearly">Yearly</option>
+                </FormSelect>
+              </div>
+            </div>
+            <div className="mt-6 mb-9">
+              <div className="pt-6 border-t border-dashed border-slate-300/70">
+                <div className="text-slate-500">
+                  Medical condition distribution
+                </div>
+                <div className="flex h-2.5 mt-3">
+                  <Tippy
+                    as="div"
+                    content="Heart Disease"
+                    className="h-full first:rounded-l last:rounded-r border border-primary/50 bg-primary/50 w-[35%]"
+                  ></Tippy>
+                  <Tippy
+                    as="div"
+                    content="Diabetes"
+                    className="h-full first:rounded-l last:rounded-r border border-info/50 bg-info/50 w-[20%]"
+                  ></Tippy>
+                  <Tippy
+                    as="div"
+                    content="Respiratory Issues"
+                    className="h-full first:rounded-l last:rounded-r border border-pending/50 bg-pending/50 w-[5%]"
+                  ></Tippy>
+                  <Tippy
+                    as="div"
+                    content="Other"
+                    className="h-full first:rounded-l last:rounded-r border border-success/50 bg-success/50 w-[40%]"
+                  ></Tippy>
+                </div>
+              </div>
+              <div className="flex justify-center mt-8">
+                <div>
+                  <div className="flex flex-col items-end">
+                    <div className="text-right truncate w-28 text-slate-500">
+                      Heart Disease
+                    </div>
+                    <div className="flex items-center mt-1.5">
+                      <div className="text-base font-medium">2,974</div>
+                      <div className="flex items-center ml-2 -mr-1 text-xs text-success">
+                        11%
+                        <Lucide icon="ChevronUp" className="w-4 h-4 ml-0.5" />
+                      </div>
+                    </div>
+                  </div>
+                  <div className="flex flex-col items-end mt-6">
+                    <div className="text-right truncate w-28 text-slate-500">
+                      Diabetes
+                    </div>
+                    <div className="flex items-center mt-1.5">
+                      <div className="text-base font-medium">1,696</div>
+                      <div className="flex items-center ml-2 -mr-1 text-xs text-success">
+                        2%
+                        <Lucide icon="ChevronUp" className="w-4 h-4 ml-0.5" />
+                      </div>
+                    </div>
+                  </div>
+                </div>
+                <div className="w-px mx-12 border-r border-dashed"></div>
+                <div>
+                  <div className="flex flex-col">
+                    <div className="truncate w-28 text-slate-500">
+                      Respiratory Issues
+                    </div>
+                    <div className="flex items-center mt-1.5">
+                      <div className="text-base font-medium">2,556</div>
+                      <div className="flex items-center ml-2 -mr-1 text-xs text-success">
+                        11%
+                        <Lucide icon="ChevronUp" className="w-4 h-4 ml-0.5" />
+                      </div>
+                    </div>
+                  </div>
+                  <div className="flex flex-col mt-6">
+                    <div className="truncate w-28 text-slate-500">Other</div>
+                    <div className="flex items-center mt-1.5">
+                      <div className="text-base font-medium">1,278</div>
+                      <div className="flex items-center ml-2 -mr-1 text-xs text-success">
+                        2%
+                        <Lucide icon="ChevronUp" className="w-4 h-4 ml-0.5" />
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <Button className="w-full border-dashed border-slate-300 hover:bg-slate-50">
+              <Lucide
+                icon="ExternalLink"
+                className="stroke-[1.3] w-4 h-4 mr-2"
+              />{" "}
+              See Full Report
+            </Button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Main;
---
+++ src/stores/dashboardSlice.ts
@@ -0,0 +1,85 @@
+import { createSlice, createAsyncThunk, PayloadAction } from "@reduxjs/toolkit";
+import { dashboardService } from "@/services/dashboard";
+import { Filer } from "@/types/dashboard";
+import { CompanyData } from "@/types/company";
+
+const name = "dashboard";
+
+export type CompanyDashboard = {
+  revenue: number;
+  profit: number;
+  employees: number;
+  filers: Filer[];
+};
+
+interface CompanySliceState {
+  companyData: CompanyData | null;
+  dashboardData: CompanyDashboard | null;
+  loading: boolean;
+  error: string | null;
+}
+
+const initialState: CompanySliceState = {
+  companyData: null,
+  dashboardData: null,
+  loading: false,
+  error: null,
+};
+
+export const fetchCompanyByName = createAsyncThunk<
+  { results: CompanyData },
+  string
+>(`${name}/fetchCompanyByName`, async (companyName: string) => {
+  return await dashboardService.fetchCompanyByName(companyName);
+});
+
+export const fetchCompanyDashboard = createAsyncThunk<
+  { results: CompanyDashboard },
+  string
+>(`${name}/fetchCompanyDashboard`, async (ticker: string) => {
+  return await dashboardService.fetchCompanyDashboard(ticker);
+});
+
+const companySlice = createSlice({
+  name,
+  initialState,
+  reducers: {},
+  extraReducers: (builder) => {
+    builder
+
+      .addCase(fetchCompanyByName.pending, (state) => {
+        state.loading = true;
+        state.error = null;
+      })
+      .addCase(
+        fetchCompanyByName.fulfilled,
+        (state, action: PayloadAction<{ results: CompanyData }>) => {
+          state.loading = false;
+          state.companyData = action.payload.results;
+        }
+      )
+      .addCase(fetchCompanyByName.rejected, (state, action) => {
+        state.loading = false;
+        state.error = action.error.message || "Failed to fetch company by name";
+      })
+
+      .addCase(fetchCompanyDashboard.pending, (state) => {
+        state.loading = true;
+        state.error = null;
+      })
+      .addCase(
+        fetchCompanyDashboard.fulfilled,
+        (state, action: PayloadAction<{ results: CompanyDashboard }>) => {
+          state.loading = false;
+          state.dashboardData = action.payload.results;
+        }
+      )
+      .addCase(fetchCompanyDashboard.rejected, (state, action) => {
+        state.loading = false;
+        state.error =
+          action.error.message || "Failed to fetch company dashboard";
+      });
+  },
+});
+
+export default companySlice.reducer;
