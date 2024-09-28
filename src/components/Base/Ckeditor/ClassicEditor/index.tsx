26185bf435754b0e2f79d17010ca7b93ca05498c
hamza <hamzasiddiq755@gmail.com>
2024-09-28 17:22:35 +0500

    revert investror profile detail

--- src/components/Base/Ckeditor/ClassicEditor/index.tsx
+++ src/components/Base/Ckeditor/ClassicEditor/index.tsx
@@ -1,10 +1,12 @@
 import "@/assets/css/vendors/ckeditor.css";
 import { createRef, useEffect, useRef } from "react";
 import ClassicEditor from "@ckeditor/ckeditor5-build-classic";
+
 import { init, updateData, CkeditorProps, CkeditorElement } from "../ckeditor";
 
+
 function Ckeditor<C extends React.ElementType = "div">({
-  disabled = false, // Use disabled to toggle read-only mode
+  disabled = false,
   config = {},
   value = "",
   onChange = () => {},
@@ -14,10 +16,8 @@ function Ckeditor<C extends React.ElementType = "div">({
   getRef = () => {},
   className,
   as,
-  hideToolbar = false,
-
   ...computedProps
-}: CkeditorProps<C> & { hideToolbar?: boolean }) {
+}: CkeditorProps<C>) {
   const props = {
     disabled: disabled,
     config: config,
@@ -33,59 +33,24 @@ function Ckeditor<C extends React.ElementType = "div">({
   const cacheData = useRef("");
   const initialRender = useRef(true);
 
-  useEffect(() => {
-    let timer : NodeJS.Timeout;
-    if (hideToolbar === true) {
-       timer = setTimeout(() => {
-        const toolbars = document.querySelectorAll(
-          ".ck.ck-toolbar"
-        ) as NodeListOf<HTMLElement>;
-        const editorOutline = document.querySelectorAll(
-          ".ck-content.ck-editor__editable_inline"
-        ) as NodeListOf<HTMLElement>;
+  console.log({value})
 
-        editorOutline.forEach((editorOutline) => {
-          editorOutline.style.border = "none";
-        });
-
-        toolbars.forEach((toolbar) => {
-          toolbar.style.display = "none";
-        });
-      }, 0);
-    }
-
-    return () => clearTimeout(timer);
-  }, []);
   useEffect(() => {
     if (editorRef.current) {
       if (initialRender.current) {
         if (props.getRef) {
           props.getRef(editorRef.current);
         }
-
-        // Hide toolbar and handle read-only mode
-        init(editorRef.current, ClassicEditor, {
-          props,
-          cacheData,
-          ...(config
-            ? {
-                config: {
-                  ...config,
-
-                  toolbar: hideToolbar ? [] : config.toolbar,
-                  readOnly: disabled,
-                },
-              }
-            : {}),
-        });
+        init(editorRef.current, ClassicEditor, { props, cacheData });
         initialRender.current = false;
       } else {
         updateData(editorRef.current, { props, cacheData });
       }
     }
-  }, [props.value, disabled, hideToolbar]); // Ensure the effect runs when hideToolbar or disabled changes
+  }, [props.value]);
 
   const Component = as || "div";
+
 
   return (
     <Component
