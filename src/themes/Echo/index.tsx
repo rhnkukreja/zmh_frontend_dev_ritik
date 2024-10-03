b40b00779d068f41f7d49ceee4ea36fd848478f7
Saad Malik <saadmalik199917@gmail.com>
2024-10-03 21:38:24 +0500

    fixes

--- src/themes/Echo/index.tsx
+++ src/themes/Echo/index.tsx
@@ -124,10 +124,10 @@ function Main() {
   return (
     <div
       className={clsx([
-        "echo group bg-gradient-to-b from-slate-200/70 to-slate-50 background relative min-h-screen",
-        "before:content-[''] before:h-[370px] before:w-screen bg-white h-7 [&.background--hidden]:before:opacity-0 before:transition-[opacity,height] before:ease-in-out before:duration-300 before:top-0 before:fixed",
+        "echo group bg-[#0000000D] relative h-auto",
+        "before:content-[''] before:h-[370px] before:w-screen bg-[#0000000D] h-7 [&.background--hidden]:before:opacity-0 before:transition-[opacity,height] before:ease-in-out before:duration-300 before:top-0 before:fixed",
         "after:content-[''] after:h-[370px] after:w-screen [&.background--hidden]:after:opacity-0 after:transition-[opacity,height] after:ease-in-out after:duration-300 after:top-0 after:fixed after:bg-texture-white after:bg-contain after:bg-fixed after:bg-[center_-13rem] after:bg-no-repeat",
-        topBarActive && "background--hidden",
+        topBarActive && "background--hidden", 'bg-[#0000000D]'
       ])}
     >
       <div
