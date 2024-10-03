1d77180898157349417d059658a55335d213d781
Saad Malik <saadmalik199917@gmail.com>
2024-10-03 22:36:48 +0500

    resolve merge conflictx.

--- src/stores/sideMenuSlice.ts
+++ src/stores/sideMenuSlice.ts
@@ -1,13 +1,6 @@
 import { createSlice } from "@reduxjs/toolkit";
 import { RootState } from "./store";
 import { icons } from "@/components/Base/Lucide";
-import caseStudyIcon from "@/assets/images/zmh-images/Case_Studies_icon.svg";
-import CompanyIcon from "@/assets/images/zmh-images/company_icon.svg";
-import EngagementIcon from "@/assets/images/zmh-images/engagement_icon.svg";
-import PeerCaseIcon from "@/assets/images/zmh-images/peer_analysis_icon.svg";
-import ShareHolderIcon from "@/assets/images/zmh-images/shareholder_icon.svg";
-import VotingIcon from "@/assets/images/zmh-images/voting_icon.svg";
-import ProfileIcon from "@/assets/images/zmh-images/profile_icon.svg";
 
 export interface Menu {
   icon: keyof typeof icons;
@@ -136,7 +129,7 @@ const initialState: any = {
     },
     "Coming Soon",
     {
-      icon: caseStudyIcon,
+      icon: 'Briefcase',
       pathname: "#",
       title: "Notes",
     },
