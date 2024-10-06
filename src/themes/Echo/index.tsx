3abccde556ceb28ba8956f341e1d081e104d809a
Saad Malik <saadmalik199917@gmail.com>
2024-10-06 15:25:39 +0500

    sidebar menu icon work

--- src/themes/Echo/index.tsx
+++ src/themes/Echo/index.tsx
@@ -248,11 +248,19 @@ function Main() {
                         setFormattedMenu([...formattedMenu]);
                       }}
                     >
-                        <Lucide
+                       { menu.title !== 'Engagement Question' && <Lucide
                           icon={menu?.icon}
                           className="side-menu__link__icon side-menu__link--active"
-                        />
+                        />}
 
+                       {
+                        menu.title === 'Engagement Question' &&
+                        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"
+                        fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"
+                        className="lucide lucide-hand-helping side-menu__link__icon side-menu__link--active">
+                        <path d="M11 12h2a2 2 0 1 0 0-4h-3c-.6 0-1.1.2-1.4.6L3 14" />
+                        <path d="m7 18 1.6-1.4c.3-.4.8-.6 1.4-.6h4c1.1 0 2.1-.4 2.8-1.2l4.6-4.4a2 2 0 0 0-2.75-2.91l-4.2 3.9" /><path d="m2 13 6 6" /></svg>
+                       }
                       <div className="side-menu__link__title link_color">{menu.title}</div>
                       {menu.badge && (
                         <div className="side-menu__link__badge">
