d84fe1987588678e8119deb74e53ff3166936cfe
Saad Malik <saadmalik199917@gmail.com>
2024-10-08 00:45:39 +0500

    Bug FIxes.

--- src/themes/Echo/index.tsx
+++ src/themes/Echo/index.tsx
@@ -160,18 +160,18 @@ function Main() {
           className={clsx([
             "h-full box border-none bg-gradient-to-b to-[#000000CC] from-[#9F1239] background rounded-none z-20 relative w-[280px] duration-300 transition-[width] group-[.side-menu--collapsed]:xl:w-[91px] group-[.side-menu--collapsed.side-menu--on-hover]:xl:shadow-[6px_0_12px_-4px_#0000000f] group-[.side-menu--collapsed.side-menu--on-hover]:xl:w-[280px] overflow-hidden flex flex-col",
           ])}
-          onMouseOver={(event) => {
-            event.preventDefault();
-            setCompactMenu(false);
-          }}
-          onMouseLeave={(event) => {
-            event.preventDefault();
-            setCompactMenu(true);
-            // toggleCompactMenu(event);  
-            // setCompactMenuOnHover(false);
-          }}
+          // onMouseOver={(event) => {
+          //   event.preventDefault();
+          //   setCompactMenu(false);
+          // }}
+          // onMouseLeave={(event) => {
+          //   event.preventDefault();
+          //   setCompactMenu(true);
+          //   // toggleCompactMenu(event);  
+          //   // setCompactMenuOnHover(false);
+          // }}
         >
-          {/* <div className={clsx([
+          <div className={clsx([
               "flex-none hidden xl:flex items-center z-10 px-5 h-[65px] w-[280px] overflow-hidden relative duration-300 group-[.side-menu--collapsed]:xl:w-[91px] group-[.side-menu--collapsed.side-menu--on-hover]:xl:w-[280px]",
             ])} >
             {
@@ -192,7 +192,7 @@ function Main() {
               <Lucide icon="X" className="w-5 h-5 stroke-[1.3] text-white" />
             </a>
             }
-          </div> */}
+          </div>
 
           <a
               href=""
