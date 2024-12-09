964b3ce56ae215f1e7dafddcf5cad1bd12dc27a3
moizaliabedin31 <moizabedin31@example.com>
2024-12-09 16:26:11 +0500

    minor changes shareholder

--- src/pages/sharedHolder/index.tsx
+++ src/pages/sharedHolder/index.tsx
@@ -966,7 +966,7 @@ function ShareHolderProposal() {
                                     className="[&_td]:last:border-b-0"
                                   >
                                     <Table.Td className="whitespace-nowrap capitalize max-w-[300px] overflow-hidden text-ellipsis text-wrap">
-                                      {noAction?.caspio_proponent_name}
+                                      {noAction?.proponent}
                                     </Table.Td>
                                     <Table.Td className="py-2  border-dashed dark:bg-darkmode-600">
                                       {noAction?.year}
@@ -1145,7 +1145,7 @@ function ShareHolderProposal() {
                                     className="[&_td]:last:border-b-0"
                                   >
                                     <Table.Td className="py-2  border-dashed dark:bg-darkmode-600">
-                                      {noAction?.caspio_proponent_name || '-'}
+                                      {noAction?.proponent || '-'}
                                     </Table.Td>
                                     <Table.Td className="py-2  border-dashed dark:bg-darkmode-600">
                                       {noAction?.year}
@@ -1257,7 +1257,7 @@ function ShareHolderProposal() {
                                     className="[&_td]:last:border-b-0"
                                   >
                                     <Table.Td className="whitespace-nowrap capitalize max-w-[300px] overflow-hidden text-ellipsis">
-                                      {noAction?.caspio_proponent_name}
+                                      {noAction?.proponent}
                                     </Table.Td>
                                     <Table.Td className="py-2  border-dashed dark:bg-darkmode-600">
                                       {noAction?.year}
