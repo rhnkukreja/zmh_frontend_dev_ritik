1a40f6e3db7ec78d5943f6ae08f5ebf0864a02cf
Saad Malik <saadmalik199917@gmail.com>
2025-02-25 22:11:15 +0500

    add meeting date on vds and npx.

--- src/pages/VdsProxyVoting/VdsProxyVotingTable.tsx
+++ src/pages/VdsProxyVoting/VdsProxyVotingTable.tsx
@@ -226,6 +226,8 @@ const VdsProxyVotingTable = () => {
       .join(", ");
     return resultString;
   };
+  const [meetingDate, setMeetingDate] = useState('');
+
 
   const [apiDropdownOptions, setApiDropdownOptions] = useState<any>([]);
 
@@ -237,6 +239,7 @@ const VdsProxyVotingTable = () => {
       if (res.result?.institution) {
         setApiDropdownOptions(res.result?.institution);
       }
+      setMeetingDate(res.result?.meeting_date);
     } catch (error) {
       return error;
     } finally {
@@ -324,8 +327,14 @@ const VdsProxyVotingTable = () => {
 
               <Tab.Panels className="mt-5">
                 <Tab.Panel className="leading-relaxed">
-                  <div className="flex justify-between  mt-1">
-                    <h1 className="text-lg font-bold">Proxy Voting</h1>
+                  <div className="flex justify-between items-center gap-4 xs:flex-col md:flex-row mb-3">
+                   <span>
+                   <h1 className="text-lg font-bold">Proxy Voting</h1>
+                    {
+                      vdsProxyDetails?.meeting_date &&
+                      <p className=" italic"> Meeting Date: {vdsProxyDetails?.meeting_date}</p>
+                    }
+                   </span>
                     {tab === "Top-20" &&
                       vdsProxyDetails?.vds_report_headers?.length > 0 && (
                         <div className="flex justify-end items-center gap-4 mb-5 xs:mt-4 md:mt-0">
@@ -617,8 +626,15 @@ const VdsProxyVotingTable = () => {
                       </div>
                     </div>
                   </form>
-                  <div className="flex justify-between mb-4 mt-1">
-                    <h1 className="text-lg font-bold mt-4">Proxy Voting</h1>
+                  <div className="flex justify-between items-center gap-4 xs:flex-col md:flex-row mb-3">
+                    <span>
+                      <h1 className="text-lg font-bold mt-4">Proxy Voting</h1>
+                      {
+                         meetingDate &&
+                        <p className=" italic"> Meeting Date: {meetingDate} </p>
+                      }
+                    </span>
+
                     {vdsProxyAllInvestorDetails?.vds_report?.length > 0 && (
                       <div className="flex justify-end items-center gap-4 xs:mt-4 md:mt-0">
                         <Tippy
