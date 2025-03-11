23db91a2d480fdfd5cf63c17186215609a9d45b3
Saad Malik <saadmalik199917@gmail.com>
2025-03-11 20:28:59 +0500

    ui icon changes.

--- src/components/InvestorCard/index.tsx
+++ src/components/InvestorCard/index.tsx
@@ -7,6 +7,7 @@ import flagIcon from "../../assets/images/zmh-images/flag-icon.png";
 import caseStudiesIcon from "../../assets/images/zmh-images/case_studies.svg";
 import investorIcon from "../../assets/images/zmh-images/investor-icon.png";
 import { MegaphoneOff } from 'lucide-react';
+import { CircleSlash2 } from 'lucide-react';
 
 import { useAppDispatch, useAppSelector } from "@/stores/hooks";
 import {
@@ -493,12 +494,31 @@ const index = () => {
                                               </div>
                                             </div>
                                           )}
-                                          {dashboard?.voted_against_directors ===
+                                          {/* {dashboard?.voted_against_directors ===
                                           'ND' && (
                                             <div className="whitespace-nowrap flex items-center justify-center">
                                             <div className="flex items-center justify-center w-full h-full text-primary mr-2">
-                                            <MegaphoneOff size={18} strokeWidth={1.2} absoluteStrokeWidth/>
+                                            <Tippy content="Not Disclose" options={{ theme: "light" }}>
+                                            <CircleSlash2 size={18} strokeWidth={2} absoluteStrokeWidth color="#b87d00"/>
+                                            </Tippy>
                                             </div>
+                                            </div>
+                                          )}
+                                            #fad900  YELLOW COLOR */}
+
+
+                                        {dashboard?.voted_against_directors ===
+                                          'ND' && (
+                                            <div className="whitespace-nowrap flex items-center justify-center">
+                                              <Tippy content="Not Disclose" options={{ theme: "light" }}>
+                                                <span
+                                                  className="bg-[#ab123d] rounded-full h-7 w-7 p-3 
+                          font-semibold text-white text-[11px] ml-2
+                           flex items-center justify-center"
+                                                >
+                                                  ND
+                                                </span>
+                                              </Tippy>
                                             </div>
                                           )}
 
@@ -513,12 +533,29 @@ const index = () => {
                                             </div>
                                           )}
 
-                                        {dashboard?.voted_against_say_on_pay ===
+                                        {/* {dashboard?.voted_against_say_on_pay ===
                                           'ND' && (
                                             <div className="whitespace-nowrap flex items-center justify-center">
                                               <div className="flex items-center justify-center w-full h-full text-primary mr-2">
+                                            <Tippy content="Not Disclose" options={{ theme: "light" }}>
                                                 <MegaphoneOff size={18} strokeWidth={1.2} absoluteStrokeWidth/>
+                                            </Tippy>
                                               </div>
+                                            </div>
+                                          )} */}
+
+{dashboard?.voted_against_directors ===
+                                          'ND' && (
+                                            <div className="whitespace-nowrap flex items-center justify-center">
+                                              <Tippy content="Not Disclose" options={{ theme: "light" }}>
+                                                <span
+                                                  className="bg-[#ab123d] rounded-full h-7 w-7 p-3 
+                          font-semibold text-white text-[11px] ml-2
+                           flex items-center justify-center"
+                                                >
+                                                  ND
+                                                </span>
+                                              </Tippy>
                                             </div>
                                           )}
                                       </Table.Td>
