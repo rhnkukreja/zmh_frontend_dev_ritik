a033a29000cb12be602c06a6f5647fce689d9f68
Bilal Shahzad <89092210+bilalshahzadd@users.noreply.github.com>
2025-09-19 19:54:16 +0500

    fix heading

--- src/pages/VdsProxyVoting/VdsProxyVotingTable.tsx
+++ src/pages/VdsProxyVoting/VdsProxyVotingTable.tsx
@@ -695,7 +695,7 @@ const VdsProxyVotingTable = () => {
                   </div>
                   <div className="flex justify-between items-center gap-4 xs:flex-col md:flex-row mb-4">
                     <div>
-                      <h1 className="text-xl font-semibold mt-4 text-slate-800 dark:text-slate-200">
+                      <h1 className="text-lg font-bold">
                         Proxy Voting
                       </h1>
                       {
