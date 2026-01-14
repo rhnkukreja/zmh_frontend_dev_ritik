da2ad70e34a36a5bfa25a4a3579877153917b577
Moiz Ali Abedin <moizabedin31@gmail.com>
2026-01-14 17:49:18 +0500

    change i icon text

--- src/themes/Echo/components/VotingDataBanner.tsx
+++ src/themes/Echo/components/VotingDataBanner.tsx
@@ -71,10 +71,12 @@ const VotingDataBanner = () => {
             <div className="flex-1 space-y-2">
               <div className="flex-1 space-y-2">
                 <p className="text-sm font-medium text-blue-800 dark:text-blue-200">
-                <strong>Analytics and charts </strong> have been added to the Dashboard.
+                  Introduced <strong>Analytics and Charts</strong> highlighting
+                  Proxy Advisory Influence and Meeting Details.
                 </p>
                 <p className="text-sm font-medium text-blue-800 dark:text-blue-200">
-                  <strong>BlackRock, Vanguard, Dimensional, and State Street </strong>Q3 2025 voting data has been added to the Dashboard.
+                  Added <strong>Q3 2025 Voting Data</strong> for BlackRock,
+                  Vanguard, Dimensional, and State Street.
                 </p>
               </div>
             </div>
