2065fe4f85b36651e0bcd3837f540853b79ec081
Moiz Ali Abedin <moizabedin31@gmail.com>
2026-02-19 18:04:30 +0500

    dev build changes

--- src/components/AGMSummaryCard/index.tsx
+++ src/components/AGMSummaryCard/index.tsx
@@ -21,7 +21,7 @@ import { Tab } from "@/components/Base/Headless";
 import { Dialog } from "@/components/Base/Headless";
 import Lucide from "@/components/Base/Lucide";
 
-const index = ({ companyGlobalSearchTicker, companyGlobalSearchName, isMeetingModal, institutionId, proxyContest = false, proxyContest2024 = false, proxyContest2025 = false, onLoaded = undefined }) => {
+const index = ({ companyGlobalSearchTicker, companyGlobalSearchName, isMeetingModal, institutionId = undefined, proxyContest = false, proxyContest2024 = false, proxyContest2025 = false, onLoaded = undefined }) => {
 
   const location = useLocation();
   const locationPathName = location?.pathname;
